ef43e81ae41cf822435f1555f8eb026a8dd9206f
JustBucket416 <26853613+JustBucket416@users.noreply.github.com>
2020-04-08 23:09:24 +0300

    smol fragment fix

--- app/src/main/java/justbucket/familiar/musicextension/MusicConfigurator.kt
+++ app/src/main/java/justbucket/familiar/musicextension/MusicConfigurator.kt
@@ -1,12 +1,12 @@
 package justbucket.familiar.musicextension
 
+import androidx.fragment.app.Fragment
 import justbucket.familiar.extension.ExtensionConfigurator
-import justbucket.familiar.extension.fragment.DetailFragment
 import justbucket.familiar.extension.model.MasterModel
 
 class MusicConfigurator : ExtensionConfigurator(EXTENSION_NAME) {
 
-    override fun configureDetailModel(masterModel: MasterModel): DetailFragment? {
+    override fun configureDetailModel(masterModel: MasterModel): Fragment? {
         return MusicFragment.newInstance(masterModel, EXTENSION_NAME)
     }
 }
